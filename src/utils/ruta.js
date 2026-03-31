ac07c4c25f330638460007c445dc558e931cfbaf
25JT <jhonierlucumimeza@gmail.com>
2026-03-31 10:20:50 -0500

    Update ruta.js

--- src/utils/ruta.js
+++ src/utils/ruta.js
@@ -1,4 +1,4 @@
 //export const ruta = "https://servidor-horalista-production.up.railway.app";
 //export const ruta = "http://localhost:3000";
-export const ruta = "http://back-horalista-pcuagy-ba45ae-157-254-174-80.traefik.me"
+export const ruta = "https://servidor.horalista.com";
 //poner view transicion entre las paginas
